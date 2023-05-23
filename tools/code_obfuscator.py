fcd7ec917ba228faf9116279979710d1a7736ab7
Stanley Zheng <szheng31@stuy.edu>
2023-05-23 11:34:58 -0400

    began work on determing where the vars are in the python code

--- tools/code_obfuscator.py
+++ tools/code_obfuscator.py
@@ -1,12 +1,28 @@
 import test
 import random
+import string
 
+f = open("test.py", "r")
+file = f.read()
+f.close()
 
 variable_names = [v for v in dir(test) if not v.startswith("__")]
 
 obfuscated_names = []
 
-for i in range(variable_names):
-    obfuscated_names.append(''.join(random.choices(string.ascii_letters, k=N)))
+for i in range(len(variable_names)):
+    obfuscated_names.append(''.join(random.choices(string.ascii_letters, k=15)))
 
-print(obfuscated_names)
+#print(obfuscated_names)
+
+def valid_var(s,tok):
+    s = s.strip()
+    if s[s.index(tok)+1] == "=" or s[s.index(tok)+1] == "," or s[s.index(tok)+1] == ")" or s[s.index(tok)+1] == ".":
+        return True
+    return False
+
+
+        
+
+
+
