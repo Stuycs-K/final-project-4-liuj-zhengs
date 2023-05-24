b02d4581984042352a6295a024e89dbd67a9cc27
Stanley Zheng <szheng31@stuy.edu>
2023-05-24 11:00:25 -0400

    finally figured out using regex how to sub var names

--- tools/code_obfuscator.py
+++ tools/code_obfuscator.py
@@ -1,6 +1,7 @@
 import test
 import random
 import string
+import re
 
 f = open("test.py", "r")
 file = f.read()
@@ -14,15 +15,21 @@ for i in range(len(variable_names)):
     obfuscated_names.append(''.join(random.choices(string.ascii_letters, k=15)))
 
 #print(obfuscated_names)
+def find(text,s):
+    indexes = []
+    index = 0
+    while index + len(s)-1 < len(text):
+        if text[index:index+len(s)] == s:
+            indexes.append(index)
+        index+=1
+    
 
-def valid_var(s,tok):
-    s = s.strip()
-    if s[s.index(tok)+1] == "=" or s[s.index(tok)+1] == "," or s[s.index(tok)+1] == ")" or s[s.index(tok)+1] == ".":
-        return True
-    return False
 
 
-        
+file = "\n" + file
+for i in range(len(variable_names)):
+    file = re.sub(r"(?<=[^.])(\b{}\b)".format(variable_names[i][0]), obfuscated_names[i], file)
 
+print(file)
 
 
