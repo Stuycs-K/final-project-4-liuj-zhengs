4737df18b7527f72313f0734718cbb26b4a146e9
Stanley Zheng <szheng31@stuy.edu>
2023-05-25 10:18:35 -0400

    added dummy code

--- tools/code_obfuscator.py
+++ tools/code_obfuscator.py
@@ -15,21 +15,20 @@ for i in range(len(variable_names)):
     obfuscated_names.append(''.join(random.choices(string.ascii_letters, k=15)))
 
 #print(obfuscated_names)
-def find(text,s):
-    indexes = []
-    index = 0
-    while index + len(s)-1 < len(text):
-        if text[index:index+len(s)] == s:
-            indexes.append(index)
-        index+=1
-    
 
 
-
+# change var names
 file = "\n" + file
 for i in range(len(variable_names)):
     file = re.sub(r"(?<=[^.])(\b{}\b)".format(variable_names[i][0]), obfuscated_names[i], file)
 
-print(file)
 
+# add dummy variables
+file = file.split("\n")
+for i in range(random.randint(50,100)):
+    randstr = str(i) + " "*random.randint(2,10) + "=" + " "*random.randint(2,10) + "poop"
+    file.insert(random.randint(0,len(file)-1),randstr)
+
+file = "\n".join(file)
+print(file)
 
